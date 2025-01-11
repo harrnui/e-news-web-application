c146bd67f1b33ef2ec836ced95a480fb8bd6c1ce
Harun <119810036+harrnui@users.noreply.github.com>
2025-01-11 17:17:48 +0100

    Translate home.js

--- kontroler/home.js
+++ kontroler/home.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const multer = require('multer');//alat za upload slika
-const upload = multer({dest: 'uploads/'});//postavljanje destinacije za spremanje slika
+const multer = require('multer');//tool for uploading pictures
+const upload = multer({dest: 'uploads/'});//destination for saving the pictures
 
-//model funkcije
+//functions from the model
 const modelRegister = require("../model/register");
 const modelLoginAdmin = require("../model/loginAdmin");
 const modelLoginAutor = require("../model/loginAutor");
@@ -18,38 +18,38 @@ const klijent = require("../config/database")
 
 
 
-//rute
+//routes
 router.get("/", (req,res)=>{
     res.sendFile(path.join(__dirname, '../pogled/index.html'));
     
 });
 
-router.post("/prijavaAdmin", (req,res)=>{
+router.post("/prijavaAdmin", (req,res)=>{ // log in Admin
     const data = req.body;
     modelLoginAdmin(data, klijent,res);
     
 });
 
-router.post('/prijavaAutor', (req,res) => {
+router.post('/prijavaAutor', (req,res) => { // log in Autor
     const data = req.body;
     modelLoginAutor(data, klijent,res);
 });
 
-router.get('/autorOpcije', (req,res) => {
-    res.sendFile(path.join(__dirname, '../pogled/other-sites/autorOpcije.html')); // treba dodati autorOpcije.html...
+router.get('/autorOpcije', (req,res) => { // autor Options
+    res.sendFile(path.join(__dirname, '../pogled/other-sites/autorOpcije.html')); 
 });
 
-router.get('/adminOpcije', (req,res) => {
+router.get('/adminOpcije', (req,res) => { // admin Options
     res.sendFile(path.join(__dirname, '../pogled/other-sites/adminOpcije.html'));
 });
 
-router.post("/registracija", (req,res) =>{
+router.post("/registracija", (req,res) =>{ // registration of a user
     const data = req.body;
-    modelRegister(data, klijent,res);//prikupljanje podataka registracije i dodavanje u bazu podataka.
+    modelRegister(data, klijent,res);//function for getting data from registration and adding them to the database. 
     
 });
 
-//rute za dobavljanje podataka
+//routes for adding data
 router.get("/adminOpcije/getData", (req,res) => {
     modelDataForClient.getDataKategorije(klijent,res);
 });
@@ -58,8 +58,8 @@ router.get("/adminOpcije/getDataPodkategorije", (req,res) => {
     modelDataForClient.getDataPodkategorije(klijent,res);
 });
 
-//ruta za objavu, metoda post
-router.post("/postObjava", upload.single('slika'), async (req,res) => {
+//route for posting news data, method get
+router.post("/postObjava", upload.single('slika'), async (req,res) => { // post a news post
     try{
         await modelDataFromClient(klijent,res,req);
     }catch(err){
@@ -67,12 +67,12 @@ router.post("/postObjava", upload.single('slika'), async (req,res) => {
     }
 });
 
-//ruta za objavu , metoda get
-router.get("/getObjava", (req,res) => {
+//route for the news post , method get 
+router.get("/getObjava", (req,res) => { // get news post
     modelDataForClient.getDataObjave(klijent,res);
 });
 
-//ruta za podkatetgorije
+//route for subcategories
 router.get("/forSubcategories/:id", (req,res) => {
     const subcategories_id = req.params.id;
     
