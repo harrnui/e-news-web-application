af8d99f60c0275ea366c343dbedb72a42e43fcf9
Harun <119810036+harrnui@users.noreply.github.com>
2025-01-11 17:53:04 +0100

    Translate getDataFromClient.js

--- model/getDataFromClient.js
+++ model/getDataFromClient.js
@@ -1,13 +1,13 @@
 
 async function insertObjava(klijent, res, req){
     
-    await klijent.query('INSERT INTO picture (picture) VALUES ($1)', [req.file.filename]);//dodavanje slike u bazu podataka
+    await klijent.query('INSERT INTO picture (picture) VALUES ($1)', [req.file.filename]);//adding pictures to the database 
 
-    const pictureID = await klijent.query('SELECT id FROM picture WHERE picture = $1', [req.file.filename]);//dobavljanje id slike iz baze podataka
+    const pictureID = await klijent.query('SELECT id FROM picture WHERE picture = $1', [req.file.filename]);//fetching pictures id to the database 
 
     
 
-    const listaSubkategorija = await klijent.query('SELECT id FROM subcategories WHERE name = $1', [req.body.podkategorija]);//dobavljanje id podkategorije iz baze podataka
+    const listaSubkategorija = await klijent.query('SELECT id FROM subcategories WHERE name = $1', [req.body.podkategorija]);//fetching  id subcategories from the database 
 
 
     if(listaSubkategorija.rows.length === 0){
@@ -15,7 +15,7 @@ async function insertObjava(klijent, res, req){
         return;
     }else{
 
-        await klijent.query('INSERT INTO news_posts (title, paragraph, content, picture, subcategories_id) VALUES ($1, $2, $3, $4, $5)', [req.body.naslov, null ,req.body.opis, pictureID.rows[0].id, listaSubkategorija.rows[0].id]);//dodavanje objave u bazu podataka
+        await klijent.query('INSERT INTO news_posts (title, paragraph, content, picture, subcategories_id) VALUES ($1, $2, $3, $4, $5)', [req.body.naslov, null ,req.body.opis, pictureID.rows[0].id, listaSubkategorija.rows[0].id]);//adding a news post to the database 
         res.send("Uspješno ste dodali objavu");
     }
 
